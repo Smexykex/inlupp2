0eda862b92ae0e21539758e4c0431f98fce080f6
Alvin Ljung Hallgren <vezon@live.se>
2024-10-21 15:56:39 +0200

    Update common

--- common.c
+++ common.c
@@ -22,3 +22,22 @@ int hash_function_void(elem_t element)
 {
   return (int) element.any;
 }
+
+bool ioopm_int_eq_function(elem_t a, elem_t b)
+{
+  return a.int_num == b.int_num;
+}
+
+bool ioopm_bool_eq_function(elem_t a, elem_t b)
+{
+    return a.boolean == b.boolean;
+}
+bool ioopm_float_eq_function(elem_t a, elem_t b)
+{
+    return a.float_num == b.float_num;
+}
+
+bool ioopm_str_eq_function(elem_t a, elem_t b)
+{
+  return strcmp(a.str, b.str) == 0;
+}
--- common.h
+++ common.h
@@ -4,6 +4,48 @@
 #include "hash_table.h"
 #include "linked_list.h"
 
+typedef union elem elem_t;
+union elem
+{
+  int int_num;
+  unsigned int unsigned_num;
+  bool boolean;
+  float float_num;
+  void *any;
+  char *str;
+};
+
+typedef bool ioopm_eq_function(elem_t a, elem_t b);
+
+bool ioopm_int_eq_function(elem_t a, elem_t b);
+bool ioopm_float_eq_function(elem_t a, elem_t b);
+bool ioopm_bool_eq_function(elem_t a, elem_t b);
+bool ioopm_str_eq_function(elem_t a, elem_t b);
+
+// an entry in a linked list
+typedef struct list_entry list_entry_t;
+typedef struct list_entry
+{
+  elem_t value; // then entry's value
+  list_entry_t *next; // a pointer to the next entry in the list
+} list_entry_t;
+
+
+// a linked list
+typedef struct list
+{
+  list_entry_t *first; // pointer to the first element in the list
+  list_entry_t *last; // pointer to the last element in the list
+  int size; // the size of the list
+  ioopm_eq_function *eq_func; // pointer to a function that compares two elements in the list
+} ioopm_list_t;
+
+typedef struct ioopm_list_iterator
+{
+  list_entry_t *current; // the current element
+  ioopm_list_t *list; // The underlying list
+} ioopm_list_iterator_t;
+
 struct merch
 {
   char *namn;
